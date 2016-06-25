74fc3a6f033922f9182046ef711870c328266e76
rokf <snewix7@gmail.com>
2016-06-25 22:58:33 +0200

    Did some modifications. :star:

--- firebase.lua
+++ firebase.lua
@@ -1,6 +1,4 @@
 
--- Inspired by the Python binding
-
 local https = require("ssl.https")
 local cjson = require("cjson")
 local ltn12 = require("ltn12")
@@ -9,88 +7,83 @@ local Firebase = {}
 
 Firebase.ROOT_URL = ''
 
-function Firebase:encode(data)  -- return data table in json string format
+function Firebase:encode(data) -- return data table in json string format
   return cjson.encode(data)
 end
 
-function Firebase:decode(json_string)  -- take json_string and return lua table
+function Firebase:decode(json_string) -- take json_string and return lua table
   return cjson.decode(json_string)
 end
 
-function Firebase:init(name)
+function Firebase:init(name) -- INIT
   self.ROOT_URL = string.format("https://%s.firebaseio.com/", name)
 end
 
-function Firebase:set(path, data)
+function Firebase:init_manual(full_url)
+  self.ROOT_URL = full_url
+end
+
+function Firebase:set(path, data) -- SET
+  local encoded = cjson.encode(data)
   https.request({
-      url = self.ROOT_URL..path..'.json',
-      method = "PUT",
-      sink = ltn12.sink.table({}),
-      source = ltn12.source.string(cjson.encode(data)),
-      headers = {
-         ["content-length"] = #cjson.encode(data),
-         ["content-type"] = "application/x-www-form-urlencoded",
-      }
+    url = self.ROOT_URL..path..'.json',
+    method = "PUT",
+    sink = ltn12.sink.table({}),
+    source = ltn12.source.string(encoded),
+    headers = {
+      ["content-length"] = #encoded,
+      ["content-type"] = "application/x-www-form-urlencoded",
+    }
   })
 end
 
-function Firebase:push(path, data)
+function Firebase:put(path, data) -- ALIAS
+  self:set(path,data)
+end
+
+function Firebase:putraw(path, rawstring) -- PUT RAW JSON
+  https.request({
+    url = self.ROOT_URL..path..'.json',
+    method = "PUT",
+    sink = ltn12.sink.table({}),
+    source = ltn12.source.string(rawstring),
+    headers = {
+      ["content-length"] = #rawstring,
+      ["content-type"] = "application/x-www-form-urlencoded",
+    }
+  })
+end
+
+function Firebase:push(path, data) -- PUSH
   https.request(self.ROOT_URL..path..'.json', cjson.encode(data))
 end
 
-function Firebase:remove(path)
+function Firebase:post(path, data) -- ALIAS
+  self:push(path,data)
+end
+
+function Firebase:pushraw(path, rawstring) -- PUSH RAW JSON
+  https.request(self.ROOT_URL..path..'.json', rawstring)
+end
+
+function Firebase:remove(path) -- REMOVE
   https.request({
-      url = self.ROOT_URL..path..'.json',
-      method = "DELETE"
+    url = self.ROOT_URL..path..'.json',
+    method = "DELETE"
   })
 end
 
--- REST API functionality
-
-function Firebase:put(path, data)
-  https.request({
-      url = self.ROOT_URL..path..'.json',
-      method = "PUT",
-      sink = ltn12.sink.table({}),
-      source = ltn12.source.string(cjson.encode(data)),
-      headers = {
-         ["content-length"] = #cjson.encode(data),
-         ["content-type"] = "application/x-www-form-urlencoded",
-      }
-  })
+function Firebase:delete(path) -- ALIAS
+  self:remove(path)
 end
 
-function Firebase:download(path,filename)
-  local file = io.open(filename, "w")
-  file:write(self:get(path, false))
-  file:close()
-end
-
-function Firebase:upload(path, filename)
-  local file = io.open(filename, "r")
-  local content_string = file:read("*all")
-  file:close()
-  https.request(self.ROOT_URL..path..'.json', content_string)
-end
-
-function Firebase:get(path, do_decoding)
+function Firebase:get(path, do_decoding) -- GET
   local URL = self.ROOT_URL .. path .. ".json"
   local result = https.request(URL)
   if do_decoding then
     return self:decode(result), "table"
   end
   return result, "string"
-end
-
-function Firebase:post(path, data)
-  https.request(self.ROOT_URL..path..'.json', cjson.encode(data))
-end
-
-function Firebase:delete(path)
-  https.request({
-      url = self.ROOT_URL..path..'.json',
-      method = "DELETE"
-  })
 end
 
 return Firebase
